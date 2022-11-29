1ce041bf68f277ab12f3fbebc78099f83d7c9ccc
Bá Lương <20020001@vnu.edu.vn>
2022-11-29 09:33:44 +0700

    Created ba branch

---
+++ frontend/src/app/components/LeftDrawer/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import FolderSharedIcon from '@mui/icons-material/FolderShared';
+import SyncLockIcon from '@mui/icons-material/SyncLock';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const UpperTexts = ['Cập nhật thông tin', 'Thời khóa biểu', 'Kết quả học tập', 'Tài liệu cá nhân'];
+const UpperIcons = [<AccountBoxIcon />, <CalendarMonthIcon />, <EmojiEventsIcon />, <FolderSharedIcon />];
+const UpperHrefs = ["/profile/information", "/profile/schedule", "/profile/learning-result", "/profile/private-document"];
+
+const LowerTexts = ['Đổi mật khẩu', 'Đăng xuất'];
+const LowerIcons = [<SyncLockIcon />, <LogoutIcon />];
+const LowerHrefs = ["/profile/change-password", "/login"];
+
+const LeftDrawer = () => {
+  return (
+    <div>
+      <Divider />
+      <List>
+        {UpperTexts.map((text, index) => (
+          <ListItem button component="a" href={UpperHrefs[index]} key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {UpperIcons[index]}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {LowerTexts.map((text, index) => (
+          <ListItem button component="a" href={LowerHrefs[index]} key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {LowerIcons[index]}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default LeftDrawer;
--- frontend/src/app/layouts/rootLayout.jsx
+++ frontend/src/app/layouts/rootLayout.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../containers/Header";
 import Footer from "../containers/Footer";
 import HomePage from "../pages/HomePage";
+import Information from "../pages/Profile/Information";
 
 const RootLayout = () => {
 	return (
@@ -15,7 +16,7 @@ const RootLayout = () => {
 				<Route path="/document" element={<Header/>} />
 				<Route path="/grade" element={<Header/>} />
 				<Route path="/validation-document" element={<Header/>} />
-				<Route path="/profile/infomation" element={<Header/>} />
+				<Route path="/profile/information" element={<Information/>} />
 				<Route path="/profile/schedule" element={<Header/>} />
 				<Route path="/profile/learning-result" element={<Header/>} />
 				<Route path="/profile/private-document" element={<Header/>} />
