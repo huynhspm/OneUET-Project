3219d1f60bd99b0f48eac30adf493a58b5b996d9
hoangha2711 <20020048@vnu.edu.vn>
2022-12-01 15:06:30 +0700

    valid page

--- frontend/src/app/layouts/rootLayout.jsx
+++ frontend/src/app/layouts/rootLayout.jsx
@@ -1,30 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "../containers/Header";
 import Footer from "../containers/Footer";
 import HomePage from "../pages/HomePage";
+import ValidationPage from "../pages/ValidationPage";
+import { createTheme } from "@mui/material";
+
+import { Box, ThemeProvider } from "@mui/system";
 
 const RootLayout = () => {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/convert" element={<Header/>} />
-				<Route path="/document" element={<Header/>} />
-				<Route path="/grade" element={<Header/>} />
-				<Route path="/validation-document" element={<Header/>} />
-				<Route path="/profile/infomation" element={<Header/>} />
-				<Route path="/profile/schedule" element={<Header/>} />
-				<Route path="/profile/learning-result" element={<Header/>} />
-				<Route path="/profile/private-document" element={<Header/>} />
-				<Route path="/profile/change-password" element={<Header/>} />
-				<Route path="/profile/login" element={<Header/>} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <BrowserRouter>
+          <Header setMode={setMode} mode={mode} />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/convert" element={<Header />} />
+            <Route path="/document" element={<Header />} />
+            <Route path="/grade" element={<Header />} />
+            <Route path="/validation-document" element={<ValidationPage />} />
+            <Route path="/profile/infomation" element={<Header />} />
+            <Route path="/profile/schedule" element={<Header />} />
+            <Route path="/profile/learning-result" element={<Header />} />
+            <Route path="/profile/private-document" element={<Header />} />
+            <Route path="/profile/change-password" element={<Header />} />
+            <Route path="/profile/login" element={<Header />} />
+          </Routes>
+
+          <Footer />
+        </BrowserRouter>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default RootLayout;
