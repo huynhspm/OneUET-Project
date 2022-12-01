2e8548c35db089f81be6cd2de01e8a5909e69fc1
phamgialinhlx <phamgialinhlx2@gmail.com>
2022-12-01 17:37:09 +0700

    fix navigation

--- frontend/src/app/containers/Header/index.jsx
+++ frontend/src/app/containers/Header/index.jsx
@@ -40,7 +40,7 @@ const Header = (props) => {
           <Link className="grade-button btn" to="/grade">
             <SchoolIcon /> Grade
           </Link>
-          <Link className="validation-button btn" to="/validation">
+          <Link className="validation-button btn" to="/validation-document">
             <FactCheckIcon /> Validation Document
           </Link>
         </div>
