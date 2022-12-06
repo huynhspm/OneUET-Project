bedcbb3dda3f5c4c4ae94e778e9ca4b5f3f6d006
Du <phamtiendu20022@gmail.com>
2022-12-06 17:07:51 +0700

    schedule + search

--- frontend/src/app/pages/Profile/Schedule/index.jsx
+++ frontend/src/app/pages/Profile/Schedule/index.jsx
@@ -1,25 +1,78 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import LeftDrawer from "../../../components/LeftDrawer";
+import {
+  Scheduler,
+  TimelineView,
+  DayView,
+  WeekView,
+  MonthView,
+  AgendaView,
+} from "@progress/kendo-react-scheduler";
+import "@progress/kendo-theme-default/dist/all.css";
+import { useState } from "react";
 
-const drawerWidth = 240;
+import { guid } from "@progress/kendo-react-common";
 
-const Schedule = () => {
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <Box
-                component="nav"
-                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-            >
-                <LeftDrawer />
-            </Box>
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-            >
-            </Box>
-        </Box>
-    );
+const meetingRooms = {
+  name: "Meeting Room",
+  data: [
+    {
+      text: "Blue room",
+      value: 1,
+      color: "blue",
+    },
+    {
+      text: "Red room",
+      value: 2,
+      color: "red",
+    },
+    {
+      text: "Green room",
+      value: 3,
+      color: "green",
+    },
+  ],
+  field: "RoomID",
+  valueField: "value",
+  textField: "text",
+  colorField: "color",
 };
 
-export default Schedule;
+const compareById = matchingItem => item => matchingItem.id === item.id;
+
+const RoomScheduler = props => {
+  const [data, setData] = useState([]);
+
+  const onDataChange = ({ created, updated, deleted }) => {
+    // Add a unique id to each new item
+    const newItemsWithIds = created.map(item => ({
+      ...item,
+      id: guid(),
+    }));
+
+    setData(dataState =>
+      dataState.reduce((acc, item) => {
+        console.log(data)
+        // Skip the item if it was deleted
+        if (deleted.find(compareById(item))) return acc;
+        // Push the updated item or current item
+        acc.push(updated.find(compareById(item)) || item);
+        return acc;
+      }, newItemsWithIds)
+    );
+  };
+
+  return (
+    <div className="k-my-8">
+      <div className="k-mb-4 k-font-weight-bold">Book a room</div>
+      <Scheduler
+        editable
+        data={data}
+        onDataChange={onDataChange}
+        resources={[meetingRooms]}
+      >
+        <WeekView />
+      </Scheduler>
+    </div>
+  );
+};
+
+export default RoomScheduler;
