fb996eb3f33cfcce5b4c5c91745d4a3bb5798a2b
huynhspm <huynhngoctrinh542002@gmail.com>
2022-12-20 19:47:46 +0700

    add data

--- backend/src/api/document/service.js
+++ backend/src/api/document/service.js
@@ -1,8 +1,13 @@
 const { Document } = require("../../database/models");
 const ResponseCode = require("../../utils/constant/ResponseCode");
-const { google } = require('googleapis');
-const fs = require('fs');
-const { client_id, client_secret, redirect_uri, refresh_token } = require("../../config");
+const { google } = require("googleapis");
+const fs = require("fs");
+const {
+	client_id,
+	client_secret,
+	redirect_uri,
+	refresh_token,
+} = require("../../config");
 
 // const CLIENT_ID = '1079743866745-5g0k7oop6760kton879oure3jkf0j45q.apps.googleusercontent.com';
 const CLIENT_ID = client_id;
@@ -13,66 +18,66 @@ const t1 = "https://docs.google.com/viewer?srcid=";
 const t2 = "&pid=explorer&efh=false&a=v&chrome=false&embedded=true";
 
 const oauth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    REDIRECT_URI,
+	CLIENT_ID,
+	CLIENT_SECRET,
+	REDIRECT_URI
 );
 
-oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 const drive = google.drive({
-    version: 'v3',
-    auth: oauth2Client
-})
+	version: "v3",
+	auth: oauth2Client,
+});
 
 async function uploadFile(name) {
 	try {
 		const response = await drive.files.create({
-                requestBody: {
-                    name: name + '.pdf',
-                    mimeType: 'application/pdf'
-                },
-                media: {
-                    mimeType: 'application/pdf',
-				body: fs.createReadStream("tmp.pdf")
-			}
-		})
+			requestBody: {
+				name: name + ".pdf",
+				mimeType: "application/pdf",
+			},
+			media: {
+				mimeType: "application/pdf",
+				body: fs.createReadStream("tmp.pdf"),
+			},
+		});
 
 		console.log(response.data);
 		// return response.data.id;
 		return generatePublicUrl(response.data.id);
 	} catch (error) {
-		console.log(error.message)
+		console.log(error.message);
 	}
 }
 
 async function generatePublicUrl(id) {
-    try {
-        // const fileId = '1rJnEYdfhkzmb1I0ij3wYldrjaro0aO6t';
-        await drive.permissions.create({
-            fileId: id,
-            requestBody: {
-                role: 'reader',
-                type: 'anyone'
-            }
-        })
-        const result = await drive.files.get({
-            fileId: id,
-            fields: 'webViewLink, webContentLink',
-        })
+	try {
+		// const fileId = '1rJnEYdfhkzmb1I0ij3wYldrjaro0aO6t';
+		await drive.permissions.create({
+			fileId: id,
+			requestBody: {
+				role: "reader",
+				type: "anyone",
+			},
+		});
+		const result = await drive.files.get({
+			fileId: id,
+			fields: "webViewLink, webContentLink",
+		});
 		// result.push({
 		// 	customViewLink: t1.concat(id, t2)
 		// });
 		// console.log(result)
 		const res = {
 			linkDownload: result.data.webContentLink,
-			linkView: t1.concat(id, t2)
+			linkView: t1.concat(id, t2),
 		};
 		return res;
-        // console.log(result.data);
-    } catch (error) {
-        console.log(error.message);
-    }
+		// console.log(result.data);
+	} catch (error) {
+		console.log(error.message);
+	}
 }
 
 const verifyDocument = async (req) => {
@@ -102,32 +107,31 @@ const verifyDocument = async (req) => {
 const createDocument = async (req) => {
 	try {
 		const newDocument = req.body;
-		let dataPDF = newDocument.dataUri.dataUri; 
-		dataPDF = dataPDF.replace('data:application/pdf;base64,', '');
-		fs.writeFile('tmp.pdf', dataPDF, 'base64', (error) => {
+		let dataPDF = newDocument.dataUri.dataUri;
+		dataPDF = dataPDF.replace("data:application/pdf;base64,", "");
+		fs.writeFile("tmp.pdf", dataPDF, "base64", (error) => {
 			if (error) throw error;
 			console.log("Doc saved!");
-		  });
-		uploadFile(String(newDocument.name))
-		.then((link) => {
-			console.log(link);
-			newDocument.linkDownload = link.linkDownload;
-			newDocument.linkView = link.linkView;
-		})
-		.then(async () => {		
-			const document = await Document.create(newDocument);
-			// console.log(newDocument);
-			const message = "Create document successfully!";
-			const status = ResponseCode.Created;
-			const data = { document };
-			
-			return {
-				data,
-				message,
-				status,
-			};
 		});
-	
+		uploadFile(String(newDocument.name))
+			.then((link) => {
+				console.log(link);
+				newDocument.linkDownload = link.linkDownload;
+				newDocument.linkView = link.linkView;
+			})
+			.then(async () => {
+				const document = await Document.create(newDocument);
+				// console.log(newDocument);
+				const message = "Create document successfully!";
+				const status = ResponseCode.Created;
+				const data = { document };
+
+				return {
+					data,
+					message,
+					status,
+				};
+			});
 	} catch (e) {
 		throw e;
 	}
@@ -222,7 +226,7 @@ const getMyDocument = async (req) => {
 				course = await document.getCourse();
 				teacher = await document.getTeacher();
 				comments = await document.getComments({
-					include: "user"
+					include: "user",
 				});
 				message = "Get my document successfully";
 				status = ResponseCode.OK;
@@ -330,6 +334,29 @@ const getDocuments = async (req) => {
 	}
 };
 
+const updateDocument = async (req) => {
+	try {
+		let { document, message, status } = await verifyDocument(req);
+		let course, teacher, comments;
+
+		if (document) {
+			document.update({ status: "public" });
+			message = "Update document successfully";
+			status = ResponseCode.OK;
+		}
+
+		const data = { document };
+
+		return {
+			data,
+			message,
+			status,
+		};
+	} catch (e) {
+		throw e;
+	}
+};
+
 const getDocument = async (req) => {
 	try {
 		let { document, message, status } = await verifyDocument(req);
@@ -393,5 +420,6 @@ module.exports = {
 	deleteMyDocument,
 	getDocuments,
 	getDocument,
+	updateDocument,
 	deleteDocument,
 };
