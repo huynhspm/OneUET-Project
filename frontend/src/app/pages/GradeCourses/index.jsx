547fdf94106b833e79bb34ca2fc75bda7393ec60
Đặng Xuân Lộc <20020057@vnu.edu.vn>
2022-12-21 14:06:02 +0700

    update courses

--- frontend/src/app/pages/GradeCourses/index.jsx
+++ frontend/src/app/pages/GradeCourses/index.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import Box from "@mui/material/Box";
 import {
   DataGrid,
   GridToolbarContainer,
@@ -10,11 +10,9 @@ import {
   GridToolbar,
 } from "@mui/x-data-grid";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { api_url } from '../../utils/config';
-import axios from 'axios';
-
-
+import { useLocation, useNavigate } from "react-router-dom";
+import { api_url } from "../../utils/config";
+import axios from "axios";
 
 const columns = [
   {
@@ -39,17 +37,6 @@ const columns = [
     headerAlign: "center",
     // width: 200,
     flex: 0.8,
-  },
-  {
-    field: "date_of_birth",
-    headerName: "Ngày sinh",
-    type: "dateTime",
-    headerAlign: "center",
-    align: "center",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    // width: 110,
-    flex: 0.7,
   },
   {
     field: "classes",
@@ -91,27 +78,176 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 2, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 3, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 4, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 5, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 6, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 7, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 8, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 9, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 10, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 11, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 12, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 13, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 14, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 15, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
-  { id: 16, code: "20020057", name: "Đặng Xuân Lộc", date_of_birth: "06/04/2002", classes: "QH-2020-I/CQ-C-CLC", midterm_grade: 8.5, final_grade: 9.5, total_grade: 9.1 },
+  {
+    id: 1,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 2,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 3,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 4,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 5,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 6,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 7,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 8,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 9,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 10,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 11,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 12,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 13,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 14,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 15,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
+  {
+    id: 16,
+    code: "20020057",
+    name: "Đặng Xuân Lộc",
+    date_of_birth: "06/04/2002",
+    classes: "QH-2020-I/CQ-C-CLC",
+    midterm_grade: 8.5,
+    final_grade: 9.5,
+    total_grade: 9.1,
+  },
 ];
 
 export default function DataGridDemo() {
   const [pageSize, setPageSize] = React.useState(5);
+  const [rows, setRows] = React.useState([]);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const id = useLocation().state.id;
+  console.log(id);
 
   function CustomToolbar() {
     return (
@@ -152,15 +288,27 @@ export default function DataGridDemo() {
   const fetchData = async () => {
     try {
       await axios
-        .get(api_url + "/api/document", {
-          params: {
-            status: "pending",
-          },
+        .get(api_url + "/api/class/" + id, {
           headers: { Authorization: `Bearer ${token}` },
         })
         .then((res) => {
-          let docs = res.data.data.documents;
-          console.log(res);
+          console.log(res.data.data.students);
+          let data = res.data.data.students;
+          let rows2 = [];
+
+          for (var i = 0; i < data.length; i++) {
+            rows2.push({
+              code: data[i].code,
+              id: i + 1,
+              name: data[i].name,
+              classes: data[i].class,
+              midterm_grade: data[i].studentClass.midterm,
+              final_grade: data[i].studentClass.final,
+              total_grade: data[i].studentClass.total,
+            });
+          }
+
+          setRows(rows2);
         });
     } catch (e) {
       console.log(e.response.data);
@@ -169,26 +317,27 @@ export default function DataGridDemo() {
 
   return (
     <>
-      <Box sx={{ height: window.innerHeight - 100, width: '100%' }}>
-        <DataGrid sx={{
-          ".MuiTablePagination-toolbar": {
-            backgroundColor: "#dee2e6"
-          },
-          ".MuiTablePagination-selectLabel, .MuiTablePagination-displayedRows": {
-            marginBottom: 0,
-            fontSize: 15
-          },
-        }}
+      <Box sx={{ height: window.innerHeight - 100, width: "100%" }}>
+        <DataGrid
+          sx={{
+            ".MuiTablePagination-toolbar": {
+              backgroundColor: "#dee2e6",
+            },
+            ".MuiTablePagination-selectLabel, .MuiTablePagination-displayedRows":
+              {
+                marginBottom: 0,
+                fontSize: 15,
+              },
+          }}
           rows={rows}
           columns={columns}
           components={{
             Toolbar: CustomToolbar,
           }}
           componentsProps={{
-
             pagination: {
-              labelRowsPerPage: ("Số hàng hiển thị:")
-            }
+              labelRowsPerPage: "Số hàng hiển thị:",
+            },
           }}
           pageSize={pageSize}
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
@@ -198,7 +347,7 @@ export default function DataGridDemo() {
           experimentalFeatures={{ newEditingApi: true }}
         />
       </Box>
-      <div style={{ height: "100px", width: "100%"}}></div>
+      <div style={{ height: "100px", width: "100%" }}></div>
     </>
   );
 }
